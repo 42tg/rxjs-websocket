8084e4142f36aa8010dc65840ff6b102ea750e1e
Tobias Graf <tobiasgraf87@gmail.com>
2019-07-29 23:25:17 +0200

    still struggling with send method

---
+++ socketExample.js
@@ -0,0 +1,21 @@
+const WebSocket = require("ws")
+const rxjs = require("rxjs")
+
+const wss = new WebSocket.Server({ port: 8080 })
+
+const interval = rxjs.interval(1000)
+
+wss.on("connection", ws => {
+    ws.on("message", message => {
+        console.log(`Received message => ${message}`)
+    })
+    const id = interval.subscribe(value => {
+        console.log(`Sending ${value}`)
+        ws.send(value)
+    })
+
+    ws.on("close", () => {
+        console.log("Dissconected")
+        id.unsubscribe()
+    })
+})
